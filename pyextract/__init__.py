3379e4e6d552e7242ca4d9539f9fd7357e6e22de
Jérome Eertmans <jeertmans@icloud.com>
2023-10-23 15:08:58 +0200

    wip: writting the CLI and linting

--- pyextract/__init__.py
+++ pyextract/__init__.py
@@ -1,39 +1,69 @@
+"""PyExtract Python package, for extracting code in modules from a node id."""
+
+import importlib
 import importlib.util
 import inspect
 import random
 import string
 import sys
-
 from pathlib import Path
-from typing import Tuple, List
+from typing import List, Tuple
 
-def extract_source_lines_from_path_and_parts(path_and_parts: str) -> Tuple[Path, List[str], int]:
+
+class BuiltInModuleError(Exception):
+    """Error when trying to read source code from a built-in module."""
+
+    def __init__(self, module: str) -> None:
+        super().__init__()
+        self.module = module
+
+    def __str__(self) -> str:
+        return f"Cannot read source code from built-in module '{self.module}'"
+
+
+def extract_source_lines_from_node_id(node_id: str) -> Tuple[Path, List[str], int]:
+    """Extract source lines of code from a given node id."""
     try:
-        path_str, parts_str = path_and_parts.split("::", maxsplit=1)
+        path_str, parts_str = node_id.split("::", maxsplit=1)
         path = Path(path_str)
         parts = parts_str.split("::")
     except ValueError:
-        path = Path(path_and_parts)
+        path = Path(node_id)
         parts = []
 
-    name = basename = path.resolve(strict=True).stem
+    try:
+        name = basename = path.resolve(strict=True).stem
 
-    while name in sys.modules:
-        name = basename + "_" + "".join(random.choice(string.ascii_letters) for i in range(8))
+        while name in sys.modules:  # To avoid name clash with already imported modules
+            name = (
+                basename
+                + "_"
+                + "".join(random.choice(string.ascii_letters) for i in range(8))
+            )
 
-    spec = importlib.util.spec_from_file_location(name, path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[name] = module
-    spec.loader.exec_module(module)
+        spec = importlib.util.spec_from_file_location(name, path)
+        assert spec is not None
+        module = importlib.util.module_from_spec(spec)
+        assert module is not None
+        sys.modules[name] = module
+        spec.loader.exec_module(module)
+
+    except FileNotFoundError:
+        name = str(path)
+        module = importlib.import_module(name)
+
     obj = module
 
     for part in parts:
         try:
             obj = getattr(obj, part)
         except AttributeError as e:
-            raise AttributeError(f"Could not find part '{part}' in '{path_and_parts}'") from e
+            raise AttributeError(f"Could not find part '{part}' in '{node_id}'") from e
 
-    lines, lineno = inspect.getsourcelines(obj)
+    try:
+        lines, lineno = inspect.getsourcelines(obj)
+
+    except TypeError as e:
+        raise BuiltInModuleError(name) from e
 
     return path, lines, lineno
-
--- pyextract/__main__.py
+++ pyextract/__main__.py
@@ -1,19 +1,100 @@
-from . import extract_source_lines_from_path_and_parts
+"""
+Command line interface (CLI) for pyextract.
 
-import sys
+To use it, please install the ``cli`` extra with ``pip install pyextract[cli]``.
+"""
 
-from textwrap import dedent
 
+import math
+import textwrap
+from typing import Optional, Sequence
+
+import click
 from pygments import highlight
-from pygments.style import Style
-from pygments.token import Token
-from pygments.lexers import Python3Lexer
 from pygments.formatters import Terminal256Formatter
+from pygments.lexers import Python3Lexer
 from pygments.styles import get_all_styles
 
+from . import extract_source_lines_from_node_id
+
+STYLES = list(get_all_styles())
+
+
+def list_styles(ctx: click.Context, param: click.Parameter, value: bool) -> None:
+    """List all supported styles from Pygments."""
+    if value:
+        click.echo("Supported styles:")
+        click.echo(textwrap.indent("\n".join(STYLES), "- "))
+        ctx.exit(0)
+
+
+@click.command()
+@click.argument("node_ids", nargs=-1, required=True)
+@click.option("-l", "--lineno", "show_lineno", is_flag=True, help="Show line number.")
+@click.option("-f", "--filename", "show_filename", is_flag=True, help="Show filename.")
+@click.option(
+    "--dedent/--no-dedent", default=True, help="Remove leading indendation from code."
+)
+@click.option(
+    "-s",
+    "--style",
+    type=click.Choice(STYLES),
+    metavar="STYLE",
+    default="default",
+    help="Set color highlighting style. "
+    "Use '--list-styles' to see all possible values.",
+)
+@click.option(
+    "--color/--no-color",
+    " / --nc",
+    type=bool,
+    default=None,
+    help="Force showing or hiding colors and other styles. By "
+    "default, it will remove color if the output does not look like "
+    "an interactive terminal.",
+)
+@click.option(
+    "--list-styles",
+    is_flag=True,
+    expose_value=False,
+    callback=list_styles,
+    help="List all supported styles and exit.",
+)
+def main(
+    node_ids: Sequence[str],
+    show_lineno: bool,
+    show_filename: bool,
+    dedent: bool,
+    style: str,
+    color: Optional[bool],
+):
+    """Extract code fragments from node ids."""
+    for _i, node_id in enumerate(node_ids):
+        path, lines, lineno = extract_source_lines_from_node_id(node_id)
+
+        code = "".join(lines)
+
+        if dedent:
+            code = textwrap.dedent(code)
+
+        code = highlight(code, Python3Lexer(), Terminal256Formatter(style=style))
+
+        if show_lineno:
+            lines = code.splitlines()
+            length = math.ceil(math.log10(len(lines) + lineno - 1))
+            lines = [
+                click.style(f"{num:{length}d}", fg="green") + ": " + line
+                for num, line in enumerate(lines, start=lineno)
+            ]
+            code = "\n".join(lines)
+
+        if show_filename:
+            code = textwrap.indent(
+                code, click.style(str(path), fg="red") + (":" if show_lineno else ": ")
+            )
+
+        click.echo(code, color=color)  # , nl=i < len(node_ids) - 1)
+
+
 if __name__ == "__main__":
-    path, lines, lineno = extract_source_lines_from_path_and_parts(sys.argv[1])
-    
-    code = dedent("".join(lines))
-    results = highlight(code, Python3Lexer(), Terminal256Formatter(style="vim"))
-    print(results, end="")
+    main()
